8d52da4ccf93fac033fca8d61f000c85d91e2ea7
Vihaan <87207836+VihaanSaini@users.noreply.github.com>
2022-02-18 20:47:42 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
-    // Discord Token \\
+    // Discord Token
     token: "Your Bot's Token",
     command: "?createautorole",
 
 
-    // Embed Settings \\
+    // Embed Settings
     embed: {
         color: "#5865F2",
         footer: "Talkative Development"
     },
 
-    // Perms \\ 
+    // Perms
     perms: "MANAGE_ROLES"
 }
 
